9880de94e2be13098e77d147d07f163dc8b79e7c
Anton <chaplyghin.94@mail.ru>
2023-02-18 19:59:38 +0300

    create Satellite class, refactor OrbitalParams class

--- buro_1440.py
+++ buro_1440.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from pymap3d import ecef2eci, geodetic2ecef
 from utilities import llh2xyz
-
+import datetime
 """
 :param G: float, гравитационная постоянная  Н*м^2*кг^(-2)
 :param ME: float, масса Земли, кг
@@ -38,12 +38,13 @@ TIME_END = 20000
 START_POS = {'x': 4362521.19692133, 'y': -
              2174459.71448059, 'z': 4720847.40402189}
 START_VEL = {'vel_x': 5356.39915538069,
-                  'vel_y': 4741.41348686709, 'vel_z': -2761.5472632395}
+             'vel_y': 4741.41348686709, 'vel_z': -2761.5472632395}
+#time = datetime.datetime(2022, 2, 18, 13, 21, 16, 584)
 TARGET_POINT = {'lat': 45.920266, 'lon': 63.342286}
 JD_START = 8084.05644194318
 
 
-class Orbit():
+class OrbitalParams():
     def __init__(self, start_pos, start_velocity):
         self._start_pos = start_pos
         self._start_velocity = start_velocity
@@ -59,9 +60,11 @@ class Orbit():
         self._ascending_node = None
         self._true_anomaly = None
         self._periapsis_arg = None
+        self._ecc_anomaly_begin = None
+        self._mean_anomaly_begin = None
+        self._average_distance = None
         self._ecc_anomaly = None
         self._mean_anomaly = None
-        self._average_distance = None
 
     def get_orbital_momentum(self):
         return self._orbital_momentum
@@ -93,135 +96,149 @@ class Orbit():
     def get_periapsis_arg(self):
         return self._periapsis_arg
 
-    def get_ecc_anomaly(self):
-        return self._ecc_anomaly
+    def get_average_distance(self):
+        return self._average_distance
+
+    def update_anomaly(self, time):
+
+        self._mean_anomaly = self._mean_anomaly_begin + time * self._average_distance
+        self._ecc_anomaly, _, _ = kepler.kepler(
+            self._mean_anomaly, self._eccentricity)
 
     def get_mean_anomaly(self):
         return self._mean_anomaly
 
-    def get_average_distance(self):
-        return self._average_distance
+    def get_ecc_anomaly(self):
+        return self._ecc_anomaly
 
 
-def construct_orbit(start_pos, start_velocity):
-    orbit = Orbit(start_pos, start_velocity)
+def construct_orbital_params(start_pos, start_velocity):
+    orbital_params = OrbitalParams(start_pos, start_velocity)
 
-    position_projections = list(orbit._start_pos.values())
-    velocity_projections = list(orbit._start_velocity.values())
+    position_projections = list(orbital_params._start_pos.values())
+    velocity_projections = list(orbital_params._start_velocity.values())
 
-    orbit._position_norm = np.linalg.norm(position_projections, ord=2)
+    orbital_params._position_norm = np.linalg.norm(position_projections, ord=2)
 
-    orbit._velocity_norm = np.linalg.norm(velocity_projections, ord=2)
+    orbital_params._velocity_norm = np.linalg.norm(velocity_projections, ord=2)
 
-    orbit._orbital_momentum = np.cross(
+    orbital_params._orbital_momentum = np.cross(
         position_projections, velocity_projections)
 
-    orbit._keplerian_energy = ((orbit._velocity_norm ** 2) / 2) - \
-        (MU / orbit._position_norm)
+    orbital_params._keplerian_energy = ((orbital_params._velocity_norm ** 2) / 2) - \
+        (MU / orbital_params._position_norm)
 
-    orbit._orbital_momentum_norm = np.linalg.norm(
-        orbit._orbital_momentum)
+    orbital_params._orbital_momentum_norm = np.linalg.norm(
+        orbital_params._orbital_momentum)
 
-    orbit._focal_distance = orbit._orbital_momentum_norm ** 2 / MU
+    orbital_params._focal_distance = orbital_params._orbital_momentum_norm ** 2 / MU
 
-    orbit._semi_major_axis = - MU / (2 * orbit._keplerian_energy)
+    orbital_params._semi_major_axis = - MU / \
+        (2 * orbital_params._keplerian_energy)
 
-    orbit._eccentricity = np.sqrt(
-        (1 + (2 * orbit._keplerian_energy) * (orbit._orbital_momentum_norm ** 2 / MU ** 2)))
+    orbital_params._eccentricity = np.sqrt(
+        (1 + (2 * orbital_params._keplerian_energy) * (orbital_params._orbital_momentum_norm ** 2 / MU ** 2)))
 
-    orbit._inclination = np.arccos(orbit._orbital_momentum[
-        2] / orbit._orbital_momentum_norm)
+    orbital_params._inclination = np.arccos(orbital_params._orbital_momentum[
+        2] / orbital_params._orbital_momentum_norm)
 
-    orbit._ascending_node = np.arctan2(orbit._orbital_momentum[
-        0], - orbit._orbital_momentum[1])
+    orbital_params._ascending_node = np.arctan2(orbital_params._orbital_momentum[
+        0], - orbital_params._orbital_momentum[1])
 
-    orbit._true_anomaly = np.arctan2(np.dot(position_projections, velocity_projections) / orbit._orbital_momentum_norm,
-                                     1 - orbit._position_norm / orbit._focal_distance)
+    orbital_params._true_anomaly = np.arctan2(np.dot(position_projections, velocity_projections) / orbital_params._orbital_momentum_norm,
+                                              1 - orbital_params._position_norm / orbital_params._focal_distance)
 
-    orbit._periapsis_arg = np.arctan2(orbit._start_pos['z'],
-                                      (orbit._start_pos['y'] * orbit._orbital_momentum[0] - orbit._start_pos['x'] * orbit._orbital_momentum[1]) / orbit._orbital_momentum_norm) - orbit._true_anomaly
+    orbital_params._periapsis_arg = np.arctan2(orbital_params._start_pos['z'],
+                                               (orbital_params._start_pos['y'] * orbital_params._orbital_momentum[0] - orbital_params._start_pos['x'] * orbital_params._orbital_momentum[1]) / orbital_params._orbital_momentum_norm) - orbital_params._true_anomaly
 
-    orbit._ecc_anomaly = np.arctan2(np.dot(position_projections, velocity_projections) /
-                                    (np.sqrt(MU * orbit._semi_major_axis)), 1 - orbit._position_norm / orbit._semi_major_axis)
+    orbital_params._ecc_anomaly_begin = np.arctan2(np.dot(position_projections, velocity_projections) /
+                                                   (np.sqrt(MU * orbital_params._semi_major_axis)), 1 - orbital_params._position_norm / orbital_params._semi_major_axis)
 
-    orbit._mean_anomaly = orbit._ecc_anomaly - np.dot(position_projections,
-                                                      velocity_projections) / (np.sqrt(MU * orbit._semi_major_axis))
+    orbital_params._mean_anomaly_begin = orbital_params._ecc_anomaly_begin - np.dot(position_projections,
+                                                                                    velocity_projections) / (np.sqrt(MU * orbital_params._semi_major_axis))
 
-    orbit._average_distance = np.sqrt(MU / orbit._semi_major_axis ** 3)
+    orbital_params._average_distance = np.sqrt(
+        MU / orbital_params._semi_major_axis ** 3)
 
-    return orbit
+    orbital_params._mean_anomaly = orbital_params._mean_anomaly_begin
+    orbital_params._ecc_anomaly = orbital_params._ecc_anomaly_begin
+
+    return orbital_params
 
 
-coordinates_X = []
-coordinates_Y = []
-coordinates_Z = []
-velocity_X = []
-velocity_Y = []
-velocity_Z = []
-Ecc = []
-orbit = construct_orbit(START_POS, START_VEL)
-periapsis_arg = orbit.get_periapsis_arg()
-inclination = orbit.get_inclination()
-ascending_node = orbit.get_ascending_node()
-eccentricity = orbit.get_eccentricity()
-semi_major_axis = orbit.get_semi_major_axis()
-true_anomaly = orbit.get_true_anomaly()
-focal_distance = orbit.get_focal_distance()
-print(inclination)
-print(ascending_node)
-print(true_anomaly)
-print(focal_distance)
-matrix = np.array([[np.cos(ascending_node) * np.cos(periapsis_arg) - np.sin(ascending_node) * np.sin(periapsis_arg) * np.cos(inclination)],
-                   [np.sin(ascending_node) * np.cos(periapsis_arg) + np.cos(ascending_node)
-                    * np.sin(periapsis_arg) * np.cos(inclination)],
-                   [np.sin(periapsis_arg) * np.sin(inclination)]])
-matrix1 = np.array([[- np.cos(ascending_node) * np.sin(periapsis_arg) - np.sin(ascending_node) * np.cos(periapsis_arg) * np.cos(inclination)],
-                    [-np.sin(ascending_node) * np.sin(periapsis_arg) + np.cos(ascending_node)
-                     * np.cos(periapsis_arg) * np.cos(inclination)],
-                    [np.cos(periapsis_arg) * np.sin(inclination)]])
+class Satellite():
+    def __init__(self, start_pos, start_vel):
+        self._orbital_params = construct_orbital_params(start_pos, start_vel)
+        self._coordinates = start_pos
+        self._velocity = start_vel
+        self._transform_matrix = None
+        self._transform_matrix1 = None
 
-Y = 2022
-M = 2
-D = 18
-t = 13 + (21 / 60) + 16.584 / 3600
-JD = 367 * Y - int(7 * (Y + int((M + 9) / 12)) / 4) - int(3 * (int((Y + (M - 9) / 7) / 100) + 1) / 4) + int(
-    275 * M / 9) + D + 1721028.5 + t / 24
-JD2020 = JD - 2451545.0
-# расчет параметров траекторного движения на заданном временном инетрвале
+    def get_params(self, delta_t):
+        self._orbital_params.update_anomaly(delta_t)
 
+        semi_major_axis = self._orbital_params.get_semi_major_axis()
+        ecc_anomaly = self._orbital_params.get_ecc_anomaly()
+        eccentricity = self._orbital_params.get_eccentricity()
+
+        x_eci, y_eci, z_eci = semi_major_axis * (self._transform_matrix * (np.cos(ecc_anomaly) - eccentricity) + self._transform_matrix1 *
+                                                 np.sqrt(1 - eccentricity ** 2) * np.sin(ecc_anomaly))
+        vel_x, vel_y, vel_z = (np.sqrt(MU / semi_major_axis) / (1 - eccentricity * np.cos(ecc_anomaly))) * (- self._transform_matrix) * np.sin(ecc_anomaly) + self._transform_matrix1 * (
+            np.sqrt(1 - eccentricity ** 2) * np.cos(ecc_anomaly))
+        julian_date = JD_START + delta_t / (3600 * 24)
+        return julian_date, x_eci, y_eci, z_eci, vel_x, vel_y, vel_z
+
+
+def construct_satellite(START_POS, START_VEL):
+    satellite = Satellite(START_POS, START_VEL)
+    ascending_node = satellite._orbital_params.get_ascending_node()
+    periapsis_arg = satellite._orbital_params.get_periapsis_arg()
+    inclination = satellite._orbital_params.get_inclination()
+    satellite._transform_matrix = np.array([[np.cos(ascending_node) * np.cos(periapsis_arg) - np.sin(ascending_node) * np.sin(periapsis_arg) * np.cos(inclination)],
+                                            [np.sin(ascending_node) * np.cos(periapsis_arg) + np.cos(ascending_node)
+                                             * np.sin(periapsis_arg) * np.cos(inclination)],
+                                            [np.sin(periapsis_arg) * np.sin(inclination)]])
+    satellite._transform_matrix1 = np.array([[- np.cos(ascending_node) * np.sin(periapsis_arg) - np.sin(ascending_node) * np.cos(periapsis_arg) * np.cos(inclination)],
+                                             [-np.sin(ascending_node) * np.sin(periapsis_arg) + np.cos(ascending_node)
+                                            * np.cos(periapsis_arg) * np.cos(inclination)],
+                                             [np.cos(periapsis_arg) * np.sin(inclination)]])
+    return satellite
+
+
+# coordinates_X = []
+# coordinates_Y = []
+# coordinates_Z = []
+# velocity_X = []
+# velocity_Y = []
+# velocity_Z = []
+# Ecc = []
 
 #  TODO: разобраться с шагом, чтобы можно было разные шаги делать, а не только delta_t=1
+
 def process(time_begin, time_end, delta_t=1):
+    satellite = construct_satellite(START_POS, START_VEL)
     with open('result.txt', 'w') as f:
         for i in range(int(time_end)):
-            M = orbit.get_mean_anomaly() + 0 * orbit.get_average_distance()
-            E, cos_true_anomaly, sin_true_anomaly = kepler.kepler(
-                M, orbit.get_eccentricity())
-            X, Y, Z = semi_major_axis * (matrix * (np.cos(E) - eccentricity) + matrix1 *
-                                         (np.sqrt(1 - eccentricity ** 2) * np.sin(E)))
+            time, x_eci, y_eci, z_eci, vel_x, vel_y, vel_z = satellite.get_params(
+                i)
 
-            # vel_x, vel_y, vel_z = (np.sqrt(MU / a) / (1 - e * np.cos(E))) * (- matrix) * np.sin(E) + matrix1 * (
-           #    np.sqrt(1 - e ** 2) * np.cos(E))
             # velocity_X.append(float(vel_x))
             # velocity_Y.append(float(vel_y))
             # velocity_Z.append(float(vel_z))
-            coordinates_X.append(float(X))
-            coordinates_Y.append(float(Y))
-            coordinates_Z.append(float(Z))
-
-            julian_date = str(JD2020 + delta_t / (3600 * 24))
-            f.write(julian_date)
-        print(X, Y, Z)
+            # coordinates_X.append(float(x_eci))
+            # coordinates_Y.append(float(y_eci))
+            # coordinates_Z.append(float(z_eci))
+            # f.write(julian_date)
 
 
 if __name__ == "__main__":
     process(TIME_BEGIN, TIME_END)
 
-# # print(E)
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.plot(coordinates_X, coordinates_Y, coordinates_Z)
-# ax.plot(vel_x, coordinates_Y, coordinates_Z)
-# plt.plot(velocity_Y)
-# plt.plot(Ecc)
-# plt.show()
+    # print(E)
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
+#     ax.plot(coordinates_X, coordinates_Y, coordinates_Z)
+#     #ax.plot(vel_x, coordinates_Y, coordinates_Z)
+#    # plt.plot((coordinates_X, coordinates_Y)
+#    # plt.plot(Ecc)
+#     plt.show()
